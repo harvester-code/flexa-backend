c4f52a241c0e98b3a2ded12f1bd313af6ddfbf2a
Vincent <vincent@datamarketing.co.kr>
2024-12-06 18:31:46 +0900

    ♻️ Change get env code

--- src/database.py
+++ src/database.py
@@ -5,17 +5,16 @@ from supabase import create_client
 
 
 def supabase_public_clinet():
-
-    url: str = os.environ.get("SUPABASE_URL")
-    key: str = os.environ.get("SUPABASE_PUBLIC_KEY")
+    url: str = os.getenv("SUPABASE_URL")
+    key: str = os.getenv("SUPABASE_PUBLIC_KEY")
     supabase = create_client(url, key)
 
     return supabase
 
 
 def supabase_auth_client():
-    url: str = os.environ.get("SUPABASE_URL")
-    key: str = os.environ.get("SUPABASE_SECRET_KEY")
+    url: str = os.getenv("SUPABASE_URL")
+    key: str = os.getenv("SUPABASE_SECRET_KEY")
     supabase = create_client(url, key)
 
     return supabase
@@ -24,9 +23,8 @@ def supabase_auth_client():
 def aws_s3_client():
     s3_client = client(
         "s3",
-        aws_access_key_id=os.environ.get("AWS_ACCESS_KEY"),
-        aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
         region_name="ap-northeast-2",
     )
-
     return s3_client
