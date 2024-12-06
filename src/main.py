d7c27fc61f733dbe19fba3b227f24fafbf11177b
Vincent <vincent@datamarketing.co.kr>
2024-12-06 18:32:21 +0900

    ♻️ Change simulation router to airports

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ from fastapi.responses import PlainTextResponse
 from starlette.middleware.cors import CORSMiddleware
 
 from src.admins.router import admins_router
+from src.airports.router import airports_router
 from src.auth.router import auth_router
 from src.managements.router import managements_router
 from src.profiles.router import profiles_router
 from src.users.router import users_router
 
-router = APIRouter()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -18,6 +18,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+router = APIRouter()
 
 
 @router.get("/", response_class=PlainTextResponse)
@@ -33,3 +35,4 @@ app.include_router(admins_router, prefix=API_PREFIX, tags=["Admins"])
 app.include_router(profiles_router, prefix=API_PREFIX, tags=["Profiles"])
 app.include_router(auth_router, prefix=API_PREFIX, tags=["Auth"])
 app.include_router(managements_router, prefix=API_PREFIX, tags=["Managements"])
+app.include_router(airports_router, prefix=API_PREFIX, tags=["Airports"])
