08c3c2bebaceb70474d69dcb39154ed6c8379c53
Tommie <tommie@datamarketing.co.kr>
2024-12-24 10:11:26 +0900

    ⚡️ Add db connection

--- src/users/router.py
+++ src/users/router.py
@@ -1,32 +1,36 @@
-from fastapi import APIRouter, HTTPException
-
-from src.users.schema import RequestAccess, UserInfo
+from fastapi import APIRouter, HTTPException, Depends
+from typing import Annotated
+from sqlalchemy.orm import Session
+from src.users.schema import RequestAccess, Certification, UserCreate
 from src.users.service import UserService
+from src.database import context_get_supabase_conn
 
-users_router = APIRouter()
+users_router = APIRouter(prefix="/users")
 users_service = UserService()
 
 # TODO: 추후 리다이렉트 URL변경 필요
 
 # TODO: 타임존 고정하는 작업 필요. -> 싱글턴패턴 참고
 
+sessions = Annotated[Session, Depends(context_get_supabase_conn)]
+
 
 # 이메일 인증
-@users_router.post("/users/certification")
-def create_certification(item: UserInfo):
+@users_router.post("/certification")
+async def create_certification(item: Certification, db: sessions):
 
-    return users_service.create_certification(item)
+    return users_service.create_certification(item=item, db=db)
 
 
-@users_router.get("/users/certification")
-def fetch_certification(id: str):
+@users_router.get("/certification")
+def fetch_certification(id: str, db: sessions):
 
-    return users_service.fetch_certification(id)
+    return users_service.fetch_certification(id, db)
 
 
 # 계정 생성
-@users_router.post("/users/create")
-def create_user(item: UserInfo):
+@users_router.post("/create")
+def create_user(item: UserCreate):
 
     if item.first_name is None or item.last_name is None or item.password is None:
         raise HTTPException(status_code=400, detail="Check Your info")
@@ -35,15 +39,15 @@ def create_user(item: UserInfo):
 
 
 # 어드민에게 승인 요청
-@users_router.post("/users/request")
-def requset_access(item: RequestAccess):
+@users_router.post("/request")
+def requset_access(item: RequestAccess, db: sessions):
 
-    return users_service.requset_access(item)
+    return users_service.requset_access(item, db)
 
 
 # 로그인
-@users_router.post("/users/login")
-def login_user(item: UserInfo):
+@users_router.post("/login")
+def login_user(item):
 
     if item.password is None:
         raise HTTPException(status_code=400, detail="Check Your password")
@@ -52,14 +56,14 @@ def login_user(item: UserInfo):
 
 
 # 로그아웃
-@users_router.post("/users/logout")
+@users_router.post("/logout")
 def logout_users():
 
     return users_service.logout_user()
 
 
 # # 초기화면 비밀번호 찾기
-# @users_router.post("/users/password/redirect")
+# @users_router.post("/password/redirect")
 # def redirect_reset_password(item: UserInfo):
 
 #     return users_service.redirect_reset_password(item)
--- src/users/schema.py
+++ src/users/schema.py
@@ -3,15 +3,18 @@ from typing import Union
 from pydantic import BaseModel
 
 
-class UserInfo(BaseModel):
+class Certification(BaseModel):
     email: str
-    first_name: Union[str, None] = None
-    last_name: Union[str, None] = None
-    password: Union[str, None] = None
+
+
+class UserCreate(BaseModel):
+    email: str
+    first_name: str
+    last_name: str
+    password: str
 
 
 class RequestAccess(BaseModel):
     user_id: str
     admin_email: str
     request_mg: str
-    request_page: str
