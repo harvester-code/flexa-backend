5b3c9e8e25a6907fe73a6c7caf8aa8ec464c2c0f
Tommie <tommie@datamarketing.co.kr>
2025-02-17 17:53:16 +0900

    💡 Add description in routers

--- src/simulation/interface/controller.py
+++ src/simulation/interface/controller.py
@@ -20,7 +20,12 @@ from src.simulation.application.service import SimulationService
 simulation_router = APIRouter(prefix="/simulations")
 
 
-@simulation_router.post("/scenario", status_code=201)
+@simulation_router.post(
+    "/scenario",
+    status_code=201,
+    summary="06_SI_001",
+    description="06_SI_001에서 new_scenario 버튼을 클릭해서 나오는 팝업창에서 빈칸을 작성한 후 create 버튼을 누르면 실행되는 API",
+)
 @inject
 async def create_scenario(
     scenario: SimulationScenarioBody,
@@ -40,7 +45,12 @@ async def create_scenario(
     )
 
 
-@simulation_router.get("/scenario", status_code=200)
+@simulation_router.get(
+    "/scenario",
+    status_code=200,
+    summary="06_SI_001",
+    description="06_SI_001에서 각 유저가 가지고 있는 시나리오 리스트를 디비에서 불러오는 API",
+)
 @inject
 async def fetch_scenario(
     user_id: str,
@@ -52,7 +62,12 @@ async def fetch_scenario(
     return await simulation_service.fetch_simulation_scenario(db, user_id)
 
 
-@simulation_router.get("/scenario/metadata", status_code=200)
+@simulation_router.get(
+    "/scenario/metadata",
+    status_code=200,
+    summary="06_SI_001",
+    description="06_SI_001에서 이미 존재하는 시나리오의 데이터를 불러오는 API",
+)
 @inject
 async def fetch_scenario_metadata(
     simulation_id: str,
@@ -64,7 +79,12 @@ async def fetch_scenario_metadata(
     return await simulation_service.fetch_scenario_metadata(db, simulation_id)
 
 
-@simulation_router.post("/scenario/metadata", status_code=201)
+@simulation_router.post(
+    "/scenario/metadata",
+    status_code=201,
+    summary="06_SI_002 ~ 021",
+    description="06_SI_002 ~ 021에서 우상단의 save버튼을 눌렀을때 각 항목의 필터값들을 저장하는 API",
+)
 @inject
 async def update_scenario_metadata(
     metadata: ScenarioMetadataBody,
@@ -87,9 +107,14 @@ async def update_scenario_metadata(
     )
 
 
-@simulation_router.post("/flight-schedule", status_code=201)
+@simulation_router.post(
+    "/flight-schedule",
+    status_code=201,
+    summary="06_SI_003, 06_SI_006",
+    description="06_SI_003에서 LOAD 버튼과 06_SI_006에서 Apply 버튼을 눌렀을 때 실행되는 API /// 만약 body값에 first_load: true로 설정이 되어있면, add_conditions에 필요한 데이터를 전달한다.",
+)
 @inject
-async def fetch_flight_schedule_chart(
+async def fetch_flight_schedule(
     flight_schedule: FlightScheduleBody,
     simulation_service: SimulationService = Depends(
         Provide[Container.simulation_service]
@@ -113,9 +138,14 @@ async def fetch_flight_schedule_chart(
     return flight_sch
 
 
-@simulation_router.post("/passenger-schedule", status_code=201)
+@simulation_router.post(
+    "/passenger-schedule",
+    status_code=201,
+    summary="06_SI_009",
+    description="06_SI_009에서 Apply 버튼을 눌렀을 때 실행되는 API",
+)
 @inject
-async def generate_passenger_schedule_chart(
+async def generate_passenger_schedule(
     passenger_schedule: PassengerScheduleBody,
     simulation_service: SimulationService = Depends(
         Provide[Container.simulation_service]
