e6d37c55a5e69bd9d7d0d69d96067f6d37d4e440
Tommie <tommie@datamarketing.co.kr>
2025-02-27 16:27:41 +0900

    🔥 Remove unnecessary comments

--- src/home/application/service.py
+++ src/home/application/service.py
@@ -5,20 +5,8 @@ from src.home.domain.repository import IHomeRepository
 class HomeService:
     """
     //매서드 정의//
-    generate : 각 controller의 최상위 매서드 (하위 매서드를 포함)
-    run_simulation : 최종 시뮬레이션 코드 (현재 분할 필요)
 
-    _calculate: 새로운 데이터 생성 (내장함수)
-    _create : 차트 생성 (내장함수)
-
-    fetch : get
-    create : post
-    update : patch / put
-    deactivate : patch (delete)
     """
-
-    # TODO: 그래프 만드는 코드들이 서로 비슷한데 합칠수는 없을까?
-    ...
 
     @inject
     def __init__(
