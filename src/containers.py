e74485e1ce9bc35f1bf8f94965a002f338641e4c
Tommie <tommie@datamarketing.co.kr>
2025-02-27 15:57:45 +0900

    🔥 Remove old router path

--- src/containers.py
+++ src/containers.py
@@ -1,7 +1,7 @@
 from dependency_injector import containers, providers
 
-from src.auth.application.service import AuthService
-from src.auth.infra.repository import AuthRepository
+# from src.auth.application.service import AuthService
+# from src.auth.infra.repository import AuthRepository
 
 from src.simulation.application.service import SimulationService
 from src.simulation.infra.repository import SimulationRepository
@@ -9,11 +9,11 @@ from src.simulation.infra.repository import SimulationRepository
 
 class Container(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(
-        packages=["src.auth", "src.simulation"],
+        packages=["src.simulation"],  # "src.auth",
     )
 
-    auth_repo = providers.Factory(AuthRepository)
-    auth_service = providers.Factory(AuthService, auth_repo=auth_repo)
+    # auth_repo = providers.Factory(AuthRepository)
+    # auth_service = providers.Factory(AuthService, auth_repo=auth_repo)
 
     simulation_repo = providers.Factory(SimulationRepository)
     simulation_service = providers.Factory(
--- src/main.py
+++ src/main.py
@@ -3,10 +3,7 @@ from fastapi import APIRouter, FastAPI
 from fastapi.responses import PlainTextResponse
 from starlette.middleware.cors import CORSMiddleware
 
-from src.airports.router import airports_router
-from src.auth.interface.controller import auth_router
 from src.containers import Container
-from src.simulation.router import simulation_router as past_simulation_router
 from src.simulation.interface.controller import simulation_router
 from src.middleware import add_middlewares
 
@@ -33,7 +30,8 @@ async def root():
 API_PREFIX = "/api/v1"
 
 app.include_router(router)
+app.include_router(simulation_router, prefix=API_PREFIX, tags=["Simulations"])
+
 # app.include_router(airports_router, prefix=API_PREFIX, tags=["Airports"])
 # app.include_router(auth_router, prefix=API_PREFIX, tags=["Auth"])
 # app.include_router(past_simulation_router, prefix=API_PREFIX, tags=["Simulations"])
-app.include_router(simulation_router, prefix=API_PREFIX, tags=["Simulations"])
