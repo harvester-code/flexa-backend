1e69fb3a22072a308e06e201045d3dd29c95c17e
Tommie <tommie@datamarketing.co.kr>
2025-03-23 12:18:53 +0900

    ➕ Add "I/D" key value in COL_FILTER_MAP

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 COL_FILTER_MAP = {
     "International/Domestic": "flight_type",
+    "I/D": "flight_type",
     "Airline": "operating_carrier_iata",
     "Region": "region_name",
     "Country": "country_code",
