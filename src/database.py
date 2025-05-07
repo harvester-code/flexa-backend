99ca88173031775af8968afb4b2a699bb4b72d4a
Vincent <vincent@datamarketing.co.kr>
2025-05-07 16:39:26 +0900

    ♻️ Refactor parquet reading to use awswrangler

--- src/database.py
+++ src/database.py
@@ -16,12 +16,7 @@ S3_SAVE_PATH = "s3://flexa-dev-ap-northeast-2-data-storage/simulations"
 
 
 def get_boto3_session() -> boto3.Session:
-    session = boto3.Session(
-        aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
-        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-        region_name="ap-northeast-2",
-    )
-    return session
+    return boto3.Session(region_name="ap-northeast-2")
 
 
 # ============================================================
