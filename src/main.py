0bb2cf3b9cfea25c89b179446688afeafe488d8c
Vincent <vincent@datamarketing.co.kr>
2025-05-08 17:11:32 +0900

    ♻️ Refactor CORS configuration to use ALLOW_ORIGINS_MAP for environment-specific origins

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 from art import text2art
 from fastapi import APIRouter, FastAPI
 from fastapi.middleware.gzip import GZipMiddleware
@@ -7,6 +9,7 @@ from mangum import Mangum
 from starlette.middleware.cors import CORSMiddleware
 
 from src.admin.interface.controller import admin_router
+from src.constants import ALLOW_ORIGINS_MAP
 from src.containers import Container
 from src.exceptions import add_exception_handlers
 from src.facility.interface.controller import facility_router
@@ -14,20 +17,13 @@ from src.home.interface.controller import home_router
 from src.middleware import add_middlewares
 from src.passenger_flow.controller import passenger_flow_router
 from src.simulation.interface.controller import simulation_router
-from src.simulation.interface.websocket_controller import ws_router
 
 app = FastAPI()
 app.container = Container()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://www.flexa.expert",
-    ],
+    allow_origins=ALLOW_ORIGINS_MAP.get(os.getenv("ENVIRONMENT")),
     allow_credentials=True,
-    # NOTE: 아래 코드는 오직 개발/테스트용
-    # allow_origins=["*"],
-    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"],
@@ -37,8 +33,7 @@ app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=5)
 add_middlewares(app)
 add_exception_handlers(app)
 
-API_PREFIX = "/api/v1"
-
+# ================================================================
 router = APIRouter()
 
 
@@ -52,20 +47,15 @@ async def health():
     return {"status": "ok"}
 
 
-# ================================
+# ================================================================
+API_PREFIX = "/api/v1"
+
 app.include_router(router, prefix=API_PREFIX)
 app.include_router(simulation_router, prefix=API_PREFIX, tags=["Simulations"])
 app.include_router(home_router, prefix=API_PREFIX, tags=["Homes"])
 app.include_router(facility_router, prefix=API_PREFIX, tags=["Detailed-Facilities"])
 app.include_router(passenger_flow_router, prefix=API_PREFIX, tags=["Passenger-Flow"])
 app.include_router(admin_router, prefix=API_PREFIX, tags=["Admins"])
-
-# ================================
-app.include_router(ws_router, prefix=API_PREFIX, tags=["ws"])
-
-# app.include_router(airports_router, prefix=API_PREFIX, tags=["Airports"])
-# app.include_router(auth_router, prefix=API_PREFIX, tags=["Auth"])
-# app.include_router(past_simulation_router, prefix=API_PREFIX, tags=["Simulations"])
 
 try:
     handler = Mangum(app)
