13fcfdea2233863795530cf9f97a3dc6cf4cc319
Tommie <tommie@datamarketing.co.kr>
2025-07-03 11:45:15 +0900

    🚧 Debug jwt

--- app/libs/jwt_decode.py
+++ app/libs/jwt_decode.py
@@ -1,15 +1,15 @@
 import os
 
-from jose import jwt
+# from jose import jwt
+import jwt
 
+# AUDIENCE = "authenticated"
 SECRET = os.getenv("SYSTEM_JWT_SECRET_KEY")
 ALGORITHM = "HS256"
-AUDIENCE = "authenticated"
 
 
 def decode_jwt(token) -> dict:
 
-    payload = jwt.decode(
-        token=token, key=SECRET, algorithms=[ALGORITHM], audience=AUDIENCE
-    )
+    payload = jwt.decode(token, SECRET, [ALGORITHM])
+
     return payload
