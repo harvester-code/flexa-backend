673f31464eb83fb918bbe6dd5a7fe93d7f5b02c8
YI <yibyungha@gmail.com>
2025-07-16 11:16:30 +0900

    ♻️ Divide S3 downloader as a singleton for connection pooling

--- app/libs/containers.py
+++ app/libs/containers.py
@@ -8,6 +8,7 @@ from app.routes.home.application.service import HomeService
 from app.routes.home.infra.repository import HomeRepository
 from app.routes.simulation.application.service import SimulationService
 from app.routes.simulation.infra.repository import SimulationRepository
+from packages.aws.s3.s3_downloader import S3Downloader
 
 
 class Container(containers.DeclarativeContainer):
@@ -20,12 +21,15 @@ class Container(containers.DeclarativeContainer):
         ]
     )
 
+    # S3 Downloader를 싱글톤으로 관리
+    s3_downloader = providers.Singleton(S3Downloader)
+
     simulation_repo = providers.Factory(SimulationRepository)
     simulation_service = providers.Factory(
         SimulationService, simulation_repo=simulation_repo
     )
 
-    home_repo = providers.Factory(HomeRepository)
+    home_repo = providers.Factory(HomeRepository, s3_downloader=s3_downloader)
     home_service = providers.Factory(HomeService, home_repo=home_repo)
 
     facility_repo = providers.Factory(FacilityRepository)
