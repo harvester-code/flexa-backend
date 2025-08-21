2829a54b3e3ffe41f362aff3248092b61ba9f5a7
YI <yibyungha@gmail.com>
2025-08-21 13:12:13 +0900

    🔧 Clean up constants by removing unused filter

--- packages/constants.py
+++ packages/constants.py
@@ -14,28 +14,5 @@ ALLOW_ORIGINS_MAP = {
 
 API_PREFIX = "/api/v1"
 
-COL_FILTER_MAP = {
-    "International/Domestic": "flight_type",
-    "I/D": "flight_type",
-    "Airline": "operating_carrier_iata",
-    "Region": "region_name",
-    "Country": "country_code",
-    "international/domestic": "flight_type",
-    "i/d": "flight_type",
-    "airline": "operating_carrier_iata",
-    "region": "region_name",
-    "country": "country_code",
-    "operating_carrier_name": "operating_carrier_name",
-    "Flight": "flight_number",
-    "flight": "flight_number",
-}
-
-CRITERIA_MAP = {
-    "operating_carrier_name": "airline",
-    "departure_terminal": "terminal",
-    "flight_type": "type",
-    "country_code": "country",
-    "region_name": "region",
-}
 
 S3_BUCKET_NAME = get_secret("AWS_S3_BUCKET_NAME")
